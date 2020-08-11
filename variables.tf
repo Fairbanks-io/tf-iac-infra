5ddf8fcc24b57c78ac95311aa6d784df6c296ec6
mgmtadmin <brandon.sorgdrager@gmail.com>
2020-08-11 14:29:25 -0500

    Include var defs

--- variables.tf
+++ variables.tf
@@ -2,3 +2,6 @@ variable "do_token" {}
 variable "pub_key" {}
 variable "pvt_key" {}
 variable "ssh_fingerprint" {}
+variable "cloudflare_email" {}
+variable "cloudflare_api_key" {}
+variable "cloudflare_zone_id" {}
