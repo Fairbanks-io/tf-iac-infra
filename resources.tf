b4554b78cedfbb996f306337bdffd0daaf6ad1c8
Jon Fairbanks <fairplay89@gmail.com>
2020-08-12 18:03:06 -0700

    Remove step

--- resources.tf
+++ resources.tf
@@ -92,7 +92,6 @@ resource "digitalocean_droplet" "jenkins-prod" {
       "docker volume create jenkins-data",
       "docker container run --name jenkins-docker --rm --detach --privileged --network jenkins --network-alias docker --env DOCKER_TLS_CERTDIR=/certs --volume jenkins-docker-certs:/certs/client --volume jenkins-data:/var/jenkins_home --publish 2376:2376 docker:dind",
       "docker container run --name jenkins-blueocean --rm --detach --network jenkins --env DOCKER_HOST=tcp://docker:2376 --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 --volume jenkins-data:/var/jenkins_home --volume jenkins-docker-certs:/certs/client:ro --publish 80:8080 --publish 443:8443 --publish 50000:50000 jenkinsci/blueocean",
-      "docker exec jenkins-blueocean cat /var/jenkins_home/secrets/initialAdminPassword"
     ]
   }
 }
