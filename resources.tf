097e2db8d38c9054fb754185bd79d2a2611a10fe
Jon Fairbanks <fairplay89@gmail.com>
2020-08-12 18:11:28 -0700

    Fix error

--- resources.tf
+++ resources.tf
@@ -91,7 +91,7 @@ resource "digitalocean_droplet" "jenkins-prod" {
       "docker volume create jenkins-docker-certs",
       "docker volume create jenkins-data",
       "docker container run --name jenkins-docker --rm --detach --privileged --network jenkins --network-alias docker --env DOCKER_TLS_CERTDIR=/certs --volume jenkins-docker-certs:/certs/client --volume jenkins-data:/var/jenkins_home --publish 2376:2376 docker:dind",
-      "docker container run --name jenkins-blueocean --rm --detach --network jenkins --env DOCKER_HOST=tcp://docker:2376 --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 --volume jenkins-data:/var/jenkins_home --volume jenkins-docker-certs:/certs/client:ro --env JENKINS_USER=${JENKINS_USER} --env JENKINS_PASS=${JENKINS_PASS} --env JAVA_OPTS=-Djenkins.install.runSetupWizard=false --publish 80:8080 --publish 443:8443 --publish 50000:50000 jenkinsci/blueocean",
+      "docker container run --name jenkins-blueocean --rm --detach --network jenkins --env DOCKER_HOST=tcp://docker:2376 --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 --volume jenkins-data:/var/jenkins_home --volume jenkins-docker-certs:/certs/client:ro --env JENKINS_USER=${var.jenkins_user} --env JENKINS_PASS=${var.jenkins_pass} --env JAVA_OPTS=-Djenkins.install.runSetupWizard=false --publish 80:8080 --publish 443:8443 --publish 50000:50000 jenkinsci/blueocean",
     ]
   }
 }
