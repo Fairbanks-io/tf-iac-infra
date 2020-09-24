9a5aa7133389220d690a0dff960fb97c8e7f6c23
Jon Fairbanks <fairplay89@gmail.com>
2020-09-23 17:32:12 -0700

    Fix

--- resources.tf
+++ resources.tf
@@ -42,11 +42,11 @@ resource "digitalocean_droplet" "vault-prod" {
   }
 }
 
-resource "cloudflare_record" "vault" {
+resource "cloudflare_record" "vault-prod" {
   zone_id = var.cloudflare_zone_id_fairbanks
   name    = "vault"
   proxied = true
-  value   = digitalocean_droplet.vault.ipv4_address
+  value   = digitalocean_droplet.vault-prod.ipv4_address
   type    = "A"
   ttl     = 1
 }
